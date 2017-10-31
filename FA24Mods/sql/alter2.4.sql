8eacc3df05b2654927ae9c8fb437b41de5dd38a2
Ap.Muthu <apmuthu@usa.net>
2017-10-31 15:38:21 +0530

    Change on debtor_trans/supp_trans keys added in 2.4/2.4rc upgrade scripts https://github.com/FrontAccountingERP/FA/commit/c0f5875022fad0f717908c17ce4e3ba42315cffc http://frontaccounting.com/punbb/viewtopic.php?pid=29341#p29341

--- FA24Mods/sql/alter2.4.sql
+++ FA24Mods/sql/alter2.4.sql
@@ -306,7 +306,7 @@ UPDATE `0_wo_requirements` r, `0_stock_moves` m
 UPDATE `0_bank_trans` SET person_id=trans_no WHERE person_type_id=26;
 
 ALTER TABLE `0_budget_trans` CHANGE `counter` `id` int(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `0_sys_prefs` CHANGE `value` `value` text NOT NULL default '';
+ALTER TABLE `0_sys_prefs` CHANGE `value` `value` text NOT NULL;
 
 ALTER TABLE `0_debtor_trans`
 	CHANGE `debtor_no` `debtor_no` int(11) unsigned NOT NULL,
