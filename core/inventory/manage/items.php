e60de8a51d65e77d15064f9a19d4f634d6530ad8
Ap.Muthu <apmuthu@usa.net>
2023-04-30 00:39:54 +0530

    New Translations file, item and fixed assets attachments and reruns

--- core/inventory/manage/items.php
+++ core/inventory/manage/items.php
@@ -38,6 +38,7 @@ page($_SESSION['page_title'], @$_REQUEST['popup'], false, "", $js);
 include_once($path_to_root . "/includes/date_functions.inc");
 include_once($path_to_root . "/includes/ui.inc");
 include_once($path_to_root . "/includes/data_checks.inc");
+include_once($path_to_root . "/includes/ui/attachment.inc");
 
 include_once($path_to_root . "/inventory/includes/inventory_db.inc");
 include_once($path_to_root . "/fixed_assets/includes/fixed_assets_db.inc");
@@ -585,7 +586,8 @@ if (!$stock_id)
 $tabs = (get_post('fixed_asset'))
 	? array(
 		'settings' => array(_('&General settings'), $stock_id),
-		'movement' => array(_('&Transactions'), $stock_id) )
+		'movement' => array(_('&Transactions'), $stock_id),
+		'attachments' => array(_('Attachments'), (user_check_access('SA_ATTACHDOCUMENT') ? get_item_code_id($stock_id) : null)))
 	: array(
 		'settings' => array(_('&General settings'), $stock_id),
 		'sales_pricing' => array(_('S&ales Pricing'), (user_check_access('SA_SALESPRICE') ? $stock_id : null)),
@@ -596,6 +598,7 @@ $tabs = (get_post('fixed_asset'))
 		'movement' => array(_('&Transactions'), (user_check_access('SA_ITEMSTRANSVIEW') && is_inventory_item($stock_id) ? 
 			$stock_id : null)),
 		'status' => array(_('&Status'), (user_check_access('SA_ITEMSSTATVIEW') ? $stock_id : null)),
+		'attachments' => array(_('Attachments'), (user_check_access('SA_ATTACHDOCUMENT') ? get_item_code_id($stock_id) : null)),
 	);
 
 tabbed_content_start('tabs', $tabs);
@@ -637,6 +640,12 @@ tabbed_content_start('tabs', $tabs);
 			$_GET['stock_id'] = $stock_id;
 			include_once($path_to_root."/inventory/inquiry/stock_status.php");
 			break;
+		case 'attachments':
+			$id = get_item_code_id($stock_id);
+			$_GET['trans_no'] = $id;
+			$_GET['type_no']= get_post('fixed_asset') ? ST_FIXEDASSET : ST_ITEM;
+			$attachments = new attachments('attachment', $id, 'items');
+			$attachments->show();
 	};
 
 br();
@@ -692,3 +701,4 @@ function generateBarcode() {
 		$tmpBarcodeID = "";	 
 	}
 }
+
